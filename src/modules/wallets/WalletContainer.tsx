2b4922e7bacbe56da70984c2012705f92bb79bdc
kaasikodes <thisbroisfresh@gmail.com>
2024-02-15 15:22:08 +0100

    chore: adds ability to add wallet from wallet page

--- src/modules/wallets/WalletContainer.tsx
+++ src/modules/wallets/WalletContainer.tsx
@@ -1,10 +1,32 @@
+import { Button, Typography } from "antd";
 import WalletsTable from "./WalletsTable";
+import { useState } from "react";
+import AddWallet from "./AddWallet";
 
+type TAction = "add-wallet";
 const WalletContainer = () => {
+  const [action, setAction] = useState<TAction>();
+  const handleAction = (action: TAction) => {
+    setAction(action);
+  };
+  const handleClose = () => {
+    setAction(undefined);
+  };
   return (
-    <div>
-      <WalletsTable />
-    </div>
+    <>
+      <AddWallet open={action === "add-wallet"} handleClose={handleClose} />
+      <div className="space-y-8">
+        <Typography.Title level={3}>Wallets</Typography.Title>
+        <div className="space-y-4">
+          <div className="flex justify-end">
+            <Button onClick={() => handleAction("add-wallet")}>
+              Add Wallet
+            </Button>
+          </div>
+          <WalletsTable />
+        </div>
+      </div>
+    </>
   );
 };
 
